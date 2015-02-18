176342e28d1120ebec5720d8cce2bdce50e30a29
Shashi Gowda <shashigowda91@gmail.com>
2015-02-18 14:35:30 +0530

    Move encode to a different file, add metadata as arg

--- src/Homework.jl
+++ src/Homework.jl
@@ -6,29 +6,31 @@ using Requires
 
 include("encode.jl")
 
+# Load javascript into IJulia
 display(MIME"text/html"(),
     """<script>$(readall(Pkg.dir("Homework", "src", "homework.js")))</script>""")
 
-function script(expr)
+script(expr) =
     display(MIME"text/html"(), string("<script>", expr, "</script>"))
-end
 
-function configure(key)
+configure(key) =
     script(string("Homework.config = ", JSON.json(key)))
-end
 
 alert(level, text) =
     "<div class='alert alert-$level'>$text</div>"
 
-teeprint(x) = begin println(x); x end # A useful debugging function
+teeprint(x) =
+    begin println(x); x end # A useful debugging function
 
-get_response_data(x) = x.body |> takebuf_string |> JSON.parse
+get_response_data(x) =
+    x.body |> takebuf_string |> JSON.parse
+
+
+#
+# Evaluate an answer.
+#
 
 function evaluate(config_json, metadata, cookie, answer)
-    # TODO: warn if user id / problem set is invalid,
-    # do whatever and decide the answer
-    # display a result (correct / not)
-    # display a button that allows user to submit the answer
 
     config = JSON.parse(config_json)
     if !haskey(config, "host")
@@ -41,7 +43,7 @@ function evaluate(config_json, metadata, cookie, answer)
 
     question_no = metadata["question"]
 
-    info = Input("<div class='alert alert-info'>Evaluating your answer...</div>")
+    info = Input(alert("info", "Evaluating your answer..."))
 
     # The HTTP requests to evaluate answer goes here...
     # After the request, you can push the
---
+++ src/encode.jl
@@ -0,0 +1,18 @@
+
+encode(metadata, x) = x
+
+encode(metadata, x::Dict) =
+    [(encode(metadata, k), encode(metadata, v)) for (k, v) in x] |> sort
+
+encode(metadata, x::Union(AbstractArray, Tuple)) =
+    map(y -> encode(metadata, y), x)
+
+function encode(metadata, x::FloatingPoint)
+    precision = get(metadata, "precision", 3)
+    round(x, precision)
+end
+
+@require SymPy begin
+    Homework.encode(metadata, x::SymPy.Sym) = "sym-" * string(x)
+end
+
