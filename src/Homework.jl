0c8e9ad972ec24e59dace1295e843c2518de19b9
Shashi Gowda <shashigowda91@gmail.com>
2015-09-16 23:25:17 +0530

    Update Homework.jl

--- src/Homework.jl
+++ src/Homework.jl
@@ -4,7 +4,7 @@ using Reactive
 
 interact_ver = try Pkg.installed("Interact") catch v"0" end
 if interact_ver != nothing && interact_ver < v"0.2.0"
-    Pkg.update()
+    Pkg.checkout("Interact")
 end
 
 using Interact
